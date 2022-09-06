31dd085a6d3744122031dc5dac89f61028d0a4ad
aledpardo <aledpviola@gmail.com>
2022-09-06 18:27:02 -0400

    Add doubly linked list implementation + tests

--- linked-list.js
+++ linked-list.js
@@ -1,10 +1,10 @@
 class Node {
-  data = {}
+  data = null
   prev = null
   next = null
 
   constructor(data) {
-    this.data = data
+    this.data = data;
   }
 
   setPrev(prev) {
@@ -15,34 +15,103 @@ class Node {
   }
 }
 
-export class LinkedList {
-  #capacity = 0
-  #size = 0
-  #head = new Node()
-  #tail = new Node()
+class LinkedList {
+  /**
+   * @type {number}
+   */
+  #capacity = 0;
+  /**
+   * @type {number}
+   */
+  #size = 0;
+  /**
+   * @type {Node}
+   */
+  #head = null;
+  /**
+   * @type {Node}
+   */
+  #tail = null;
 
   constructor(capacity = 0) {
-    this.#capacity = capacity
-    this.#size = 0
+    this.#capacity = capacity;
   }
 
-  append(node = new Node()) {
+  /**
+   * Appends `data` to the end of list
+   * @param {T} data
+   * @returns {Node}
+   */
+  append(data) {
+    const node = new Node(data);
     if (this.#size === this.#capacity) {
-      this.remove(this.#head)
+      this.remove(this.#head);
     }
-    node.setPrev(this.#tail)
-    node.setNext(null)
-    this.#tail.setNext(node)
-    this.#tail = node
+    if (this.#size === 0) {
+      this.#head = node;
+      this.#tail = node;
+    } else  if (this.#size === 1) {
+      this.#head.setNext(node);
+      node.setPrev(this.#head);
+      this.#tail = node;
+    } else {
+      this.#tail.setNext(node);
+      node.setPrev(this.#tail);
+    }
+    this.#tail = node;
+    this.#size++;
+    return node;
   }
 
-  remove(node = new Node()) {
+  /**
+   * Removes node from list
+   * @param {Node} node
+   * @returns {boolean}
+   */
+  remove(node) {
+    if (node == null) {
+      return false;
+    }
     if (node === this.#head) {
       this.#head = this.#head.next
-      this.#head.setPrev(null)
-      return
+      if (this.#head != null) {
+        this.#head.setPrev(null);
+      }
+      this.#size--;
+      return true;
     }
-    node.prev.setNext(node.next)
-    node.next.setPrev(node.prev)
+    node.prev.setNext(node.next);
+    node.next.setPrev(node.prev);
+    this.#size--;
+    return true;
+  }
+
+  /**
+   * Finds a {Node} by reference
+   * @param {Node} node
+   * @returns Node
+   */
+  find(data) {
+    for (const iter of this) {
+      if (Boolean(iter) && iter.data === data) {
+        return iter;
+      }
+    }
+    return null;
+  }
+
+  /**
+   *
+   * @returns {Iterable<Node>}
+   */
+  *[Symbol.iterator]() {
+    let iter = this.#head;;
+    do {
+      yield iter;
+      iter = iter?.next;
+    } while(iter != null);
+    return null;
   }
 }
+
+module.exports = LinkedList;
---
+++ linked-list.test.js
@@ -0,0 +1,66 @@
+const assert = require('assert');
+const LinkedList = require('./linked-list');
+const test = require('./test');
+
+test('instantiate LinkedList', () => {
+  const ll = new LinkedList(0);
+  assert(ll instanceof LinkedList, 'not an instanceof LinkedList');
+});
+
+test('append', () => {
+  const ll = new LinkedList(1);
+  const nd = ll.append(0);
+  assert(nd != null, `failed to append`);
+  const fd = ll.find(nd.data);
+  assert(fd === nd, `failed to find node after append (check by ref)`);
+  assert(fd.data === nd.data, `failed to find node after append (check by data)`);
+});
+
+test('iterate', () => {
+  const ll = new LinkedList(6);
+  const ar = [];
+  ll.append(ar.push(ar.length) - 1);
+  ll.append(ar.push(ar.length) - 1);
+  ll.append(ar.push(ar.length) - 1);
+  ll.append(ar.push(ar.length) - 1);
+  ll.append(ar.push(ar.length) - 1);
+  for (const i of ll) {
+    const expected = ar.shift();
+    // console.log(i.data, expected);
+    assert(i.data === expected, `incorrect iterate order`);
+  }
+  const rest = [...ll];
+  assert(Array.isArray(rest), `rest operator failed`);
+});
+
+test('find', () => {
+  const ll = new LinkedList(4);
+  const ar = [];
+  ar.push(ll.append(0));
+  ar.push(ll.append(1));
+  ar.push(ll.append(2));
+  ar.push(ll.append(3));
+  for (const i of ar) {
+    const f = ll.find(i.data);
+    assert(f != null, `could not find node with data ${i.data}`);
+  }
+  const last = ll.append(4);
+  const notFound = ll.find(0);
+  assert(notFound == null, 'first appended should have been removed');
+  const found = ll.find(4);
+  assert(found === last, `find expected 4, found ${found} (check by ref)`);
+  assert(found.data === last.data, `find expected 4, found ${found} (check by data)`);
+});
+
+test('remove', () => {
+  const ll = new LinkedList(4);
+  const ar = [];
+  ar.push(ll.append(0));
+  ar.push(ll.append(1));
+  ar.push(ll.append(2));
+  ar.push(ll.append(3));
+  for (const it of ar) {
+    assert(ll.remove(it), `failed to remove ${it.data}`);
+    assert(ll.find(it.data) == null, 'still found data after being removed');
+  }
+});
